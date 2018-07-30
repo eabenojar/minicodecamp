58e00ad34a746e51b512533ce75f6b79e00a1aa6
Eric Abenojar <abenoeric@gmail.com>
2018-07-29 23:01:37 -0500

    Added reponsive design to admin dashboard

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,50 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "../styles/header.css";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
 
 class Header extends Component {
+  state = {
+    isOpen: false
+  };
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
   render() {
     return (
       <div className="header-container">
-        <div className="header-section">
+        <div className="header-sub-container">
+          <Navbar color="light" light expand="md" navbar>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="d-flex align-items-start" navbar>
+                <NavItem>
+                  <NavLink href="/course/">Courses</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/course">Quizes</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+            <NavbarBrand href="/course">Mini Code Camp</NavbarBrand>
+          </Navbar>
+        </div>
+
+        {/* <div className="header-section">
           <div className="header-section-left">
             <Link className="header-link" to="/course">
               Courses
@@ -23,7 +61,7 @@ class Header extends Component {
               Sign in
             </Link>
           </div>
-        </div>
+        </div> */}
       </div>
     );
   }
